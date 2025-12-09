596d14a5602bd2de913f7483a05a27ba02bbe71c
Srijan Sarkar <srijanb463@gmail.com>
2025-12-10 00:37:43 +0530

    added the frontend for login and signup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import CommandCenter from './pages/CommandCenter';
+import LoginPage from './pages/Login';
+import SignUpPage from './pages/Signup';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Navigate to="/command-center" replace />} />
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/sign-up" element={<SignUpPage />} />
         <Route path="/command-center" element={<CommandCenter />} />
         {/* Add more routes here as needed */}
         {/* <Route path="/performance" element={<Performance />} /> */}
